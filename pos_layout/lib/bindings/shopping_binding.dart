1476ed558f4206f32b5ee010481bc3c8848fb0bf
Chong <monkey.d.chong@gmail.com>
2022-04-27 22:47:59 +0700

    init

--- pos_layout/lib/bindings/shopping_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:pos_layout/controllers/payment_controller.dart';
-
-class ShoppingBinding extends Bindings {
-  @override
-  void dependencies() async {
-    Get.lazyPut(() => PaymentController());
-  }
-}
