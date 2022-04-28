d8903841505b067ee799a42f08115f86700c5fad
Chong <99638559+puchongdev@users.noreply.github.com>
2022-04-28 13:10:38 +0700

    update

--- pos_android/lib/screens/splash_screen.dart
+++ pos_android/lib/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_device_type/flutter_device_type.dart';
 import 'package:pos_android/constants/styles.dart';
 import 'package:pos_android/routes/app_pages.dart';
 import 'package:flutter/material.dart';
@@ -10,9 +11,6 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     _validateAuthenticationAndRedirect(context);
 
-    if(!context.isTablet ){
-      Get.offAllNamed(Routes.errorDevice);
-    }
 
     return Scaffold(
       backgroundColor: Styles.mainColor,
@@ -25,13 +23,12 @@ class SplashScreen extends StatelessWidget {
   void _validateAuthenticationAndRedirect(BuildContext context) async {
     await Future.delayed(const Duration(seconds: 3), () {});
 
-    Get.offAllNamed(Routes.login);
     //
-    // if( !context.isTablet ){
-    //   Get.offAllNamed(Routes.errorDevice);
-    // }
-    // else {
-    //   Get.offAllNamed(Routes.login);
-    // }
+    if(!Device.get().isTablet ){
+      Get.offAllNamed(Routes.errorDevice);
+    }
+    else {
+      Get.offAllNamed(Routes.login);
+    }
   }
 }
